89c1d6a3762b47eed7dca83dd9424b7639d90234
epaew <epaew.333@gmail.com>
2019-11-09 10:41:42 +0900

    update_helix_make_rule

--- keyboards/helix/rev2/keymaps/edvorakjp/rules.mk
+++ keyboards/helix/rev2/keymaps/edvorakjp/rules.mk
@@ -11,10 +11,10 @@ TAP_DANCE_ENABLE = yes
 # Helix Spacific Build Options
 # you can uncomment and edit follows 7 Variables
 #  jp: 以下の7つの変数を必要に応じて編集し、コメントアウトをはずします。
-# HELIX_ROWS = 5              # Helix Rows is 4 or 5
-# OLED_ENABLE = no            # OLED_ENABLE
+HELIX_ROWS = 4              # Helix Rows is 4 or 5
+OLED_ENABLE = yes            # OLED_ENABLE
 # LOCAL_GLCDFONT = no         # use each keymaps "helixfont.h" instead of "common/glcdfont.c"
-# LED_BACK_ENABLE = no        # LED backlight (Enable WS2812 RGB underlight.)
+LED_BACK_ENABLE = yes        # LED backlight (Enable WS2812 RGB underlight.)
 # LED_UNDERGLOW_ENABLE = no   # LED underglow (Enable WS2812 RGB underlight.)
 # LED_ANIMATIONS = yes        # LED animations
 # IOS_DEVICE_ENABLE = no      # connect to IOS device (iPad,iPhone)
